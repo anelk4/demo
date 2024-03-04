2ce36d299355c6c8c46398784ddda5b94c47a2f3
Bakyt Zhansaya <zansaabakyt364@gmail.com>
2024-03-04 22:14:08 +0600

    Revert "register re"

--- src/main/java/com/example/demo/services/AccountService.java
+++ src/main/java/com/example/demo/services/AccountService.java
@@ -1,19 +1,8 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Account;
-import com.example.demo.repositories.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class AccountService{
-    @Autowired
-    private AccountRepository accountRepository;
-
-
-
-    public Account registerNewAccount(Account account) {
-        // Here you can add encryption to the password before saving
-        return accountRepository.save(account);
-    }
+public interface AccountService {
+    void save(Account account);
+    Account findByUsername(String username);
 }
